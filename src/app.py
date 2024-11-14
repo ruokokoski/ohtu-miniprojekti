5f3dce1c7a4a2d7ef97594cff98a7cf7aef8c174
l-antti <laura.k.anttila@gmail.com>
2024-11-14 16:56:38 +0200

    List all references

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 from flask import render_template#, redirect, request, jsonify, flash
 #from db_helper import reset_db
 from config import app#, test_env
+from repositories.reference_repository import list_references
+
 
 @app.route("/")
 def index():
@@ -10,3 +12,9 @@ def index():
 @app.route("/new_reference")
 def new():
     return render_template("new_reference.html")
+
+# need to create db/table first..
+#@app.route("/references")
+#def browse_references():
+    #references_list = list_references()
+    #return render_template("list_references.html", references=references_list)
--- src/repositories/reference_repository.py
+++ src/repositories/reference_repository.py
@@ -1,5 +1,5 @@
-#from config import db
-#from sqlalchemy import text
+from config import db
+from sqlalchemy import text
 #from entities.reference import Reference
 
 def get_references():
@@ -8,6 +8,14 @@ def get_references():
     #result = db.session.execute(sql_query)
     #references = result.fetchall()
     #return
+
+def list_references(): # need to create the database first
+    pass
+    #sql = text('SELECT author, title FROM "references" ORDER BY author DESC')
+    #result = db.session.execute(sql).fetchall()
+    #return result
+
+
 
 '''
 def create_reference(content):
