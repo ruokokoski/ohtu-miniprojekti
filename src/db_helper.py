81def8a5c378305f0cfc8a4ad2ff46a28a7a6cf5
win <samuli.siukonen@gmail.com>
2024-11-18 20:37:43 +0200

    fix db_helper indentations

--- src/db_helper.py
+++ src/db_helper.py
@@ -1,53 +1,50 @@
-from config import db, app
 from sqlalchemy import text
+from config import db, app
 
 table_name = "Books"
 
 def table_exists(name):
-  sql_table_existence = text(
-    "SELECT EXISTS ("
-    "  SELECT 1"
-    "  FROM information_schema.tables"
-    f" WHERE table_name = '{name}'"
-    ")"
-  )
-
-  print(f"Checking if table {name} exists")
-  print(sql_table_existence)
-
-  result = db.session.execute(sql_table_existence)
-  return result.fetchall()[0][0]
+    sql_table_existence = text(
+		"  SELECT EXISTS (  "
+		"  SELECT 1  "
+		"  FROM information_schema.tables  "
+		f" WHERE table_name = '{name}'"
+		"  )  "
+	)
+    print(f"Checking if table {name} exists")
+    print(sql_table_existence)
+    result = db.session.execute(sql_table_existence)
+    return result.fetchall()[0][0]
 
 def reset_db():
-  print(f"Clearing contents from table {table_name}")
-  sql = text(f"DELETE FROM {table_name}")
-  db.session.execute(sql)
-  db.session.commit()
-
-def setup_db():
-  if table_exists(table_name):
-    print(f"Table {table_name} exists, dropping")
-    sql = text(f"DROP TABLE {table_name}")
+    print(f"Clearing contents from table {table_name}")
+    sql = text(f"DELETE FROM {table_name}")
     db.session.execute(sql)
     db.session.commit()
 
-  print(f"Creating table {table_name}")
-  sql = text(
-    f"CREATE TABLE {table_name} ("
-    "  id SERIAL PRIMARY KEY,  "
-    "  key TEXT NOT NULL,  "
-    "  author TEXT NOT NULL,  "
-    "  year INTEGER NOT NULL,  "
-    "  title TEXT NOT NULL,  "
-    "  publisher TEXT,  "
-    "  address TEXT"
-    ")"
-  )
+def setup_db():
+    if table_exists(table_name):
+        print(f"Table {table_name} exists, dropping")
+        sql = text(f"DROP TABLE {table_name}")
+        db.session.execute(sql)
+        db.session.commit()
 
-  db.session.execute(sql)
-  db.session.commit()
+    print(f"Creating table {table_name}")
+    sql = text(
+		f" CREATE TABLE {table_name} ("
+		"  id SERIAL PRIMARY KEY,  "
+		"  key TEXT NOT NULL,  "
+		"  author TEXT NOT NULL,  "
+		"  year INTEGER NOT NULL,  "
+		"  title TEXT NOT NULL,  "
+		"  publisher TEXT,  "
+		"  address TEXT"
+		")"
+	)
+
+    db.session.execute(sql)
+    db.session.commit()
 
 if __name__ == "__main__":
     with app.app_context():
-      setup_db()
-
+        setup_db()
