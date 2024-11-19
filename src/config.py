4dad880967e984c8d533d351dfdef6ed61537961
Teemu Ruokokoski <teemu.ruokokoski@helsinki.fi>
2024-11-19 08:28:15 +0200

    Test pipeline

--- src/config.py
+++ src/config.py
@@ -6,9 +6,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 test_env = getenv("TEST_ENV") == "true"
+ci_env = getenv("CI") == "true"
 print(f"Test environment: {test_env}")
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+#app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+if ci_env:
+    app.config["SQLALCHEMY_DATABASE_URI"] = getenv("TEST_DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
